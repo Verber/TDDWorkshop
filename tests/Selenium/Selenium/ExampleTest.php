f66b7e45f5cfc1e4dd136e7636e049cc10d368d3
Ivan Mosiev <i.k.mosev@gmail.com>
2012-11-15 04:30:41 +0200

    Chnaged selenium,added jar

---
+++ tests/Selenium/Selenium/ExampleTest.php
@@ -0,0 +1,25 @@
+<?php
+class ExampleTest extends PHPUnit_Extensions_Selenium2TestCase
+{
+    protected function setUp()
+    {
+        $this->setBrowser('firefox');
+        $this->setBrowserUrl('http://www.google.com.ua/');
+    }
+
+    public function testTitle()
+    {
+        $this->open('/');
+        $this->assertTitle('Google');
+    }
+
+    public function testPhpunit()
+    {
+        $this->open('/');
+        $this->type('name=q', 'phpunit');
+        $this->click('name=btnG');
+        sleep(3);
+        $this->assertElementContainsText('id=ires', 'PHPUnit');
+    }
+
+}
